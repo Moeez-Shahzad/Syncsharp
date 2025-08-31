c6e7a16a2b13608491764380c63c300a98653225
Moeez Shahzad <moeez.shahzad2002@gmail.com>
2025-08-31 14:37:14 +0500

    Logo Update footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "class", // ✅ important for ThemeProvider toggle
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
